6bde9b0a2b771a16c846b42f7d21ca62dc4768f1
Tanja Bast <tanja.s.bast@gmail.com>
2020-01-12 21:02:32 +0100

    Import embroidery patterns from pyembroidery

---
+++ tests/test_embroidery_import.py
@@ -0,0 +1,32 @@
+from pyembroidery import EmbPattern, COLOR_CHANGE, write_vp3
+
+from file_io.embroidery_import import from_pyembroidery
+from lib.functions_2d import line
+from lib.sample import sample
+
+
+def test_from_pyembroidery():
+    # create embroidery pattern with two blocks
+    pattern = EmbPattern()
+
+    stitches1 = sample(line(90, 0), 10)
+    stitches1 = stitches1.tolist()
+    pattern.add_block(stitches1, 0x808080)
+
+    # ToDo
+    # manual color change seems to be necessary here, otherwise pattern.get_as_colorblocks()
+    # will not return two separate color bocks, although pyembroidery adds COLOR_BREAK
+    # automatically after each block. Check if there is a better way to add separate color blocks
+    pattern.color_change()
+
+    stitches2 = sample(line(0, 90), 10)
+    stitches2 = stitches2.tolist()
+    pattern.add_block(stitches2, 0x000000)
+
+    # convert from pyembroidery format
+    import_pattern = from_pyembroidery(pattern)
+
+    # expect one stitch for each stitch plus one COLOR_BREAK for each block
+    # plus one for the manual color change
+    assert len(pattern.stitches) == import_pattern.number_of_stitches + len(import_pattern.stitch_blocks) + 1
+
