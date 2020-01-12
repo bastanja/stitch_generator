6f9dc7067c2cc74e09dac7c25fb6cf3ce45bffc3
Tanja Bast <tanja.s.bast@gmail.com>
2020-01-12 17:32:47 +0100

    Add functionality to export EmbroideryPattern with pyembroidery

---
+++ file_io/embroidery_export.py
@@ -0,0 +1,20 @@
+from pyembroidery import EmbPattern, write_vp3, CONTINGENCY_TIE_ON_THREE_SMALL, CONTINGENCY_TIE_OFF_THREE_SMALL
+from lib.embroidery_pattern import EmbroideryPattern
+
+
+def to_pyembroidery(embroidery_pattern: EmbroideryPattern):
+    scale_factor = 10  # convert from millimeters to embroidery file scale 1/10 mm
+    result = EmbPattern()
+
+    for i in range(len(embroidery_pattern.stitch_blocks)):
+        scaled_stitches = embroidery_pattern.stitch_blocks[i] * scale_factor
+        result.add_block(scaled_stitches.tolist(), embroidery_pattern.colors[i])
+
+    return result
+
+
+export_parameters = {"tie_on": CONTINGENCY_TIE_ON_THREE_SMALL, "tie_off": CONTINGENCY_TIE_OFF_THREE_SMALL}
+
+
+def export_vp3(pattern: EmbroideryPattern, file_path: str):
+    write_vp3(to_pyembroidery(pattern), file_path, export_parameters)
---
+++ tests/test_embroidery_export.py
@@ -0,0 +1,22 @@
+from pyembroidery import COMMAND_MASK, COLOR_BREAK
+from file_io.embroidery_export import to_pyembroidery
+from lib.embroidery_pattern import EmbroideryPattern
+from lib.functions_2d import line
+from lib.sample import sample
+
+
+def test_to_pyembroidery():
+    # create embroidery pattern with two blocks
+    pattern = EmbroideryPattern()
+    pattern.add_stitches(sample(line(100, 0)))
+    pattern.add_stitches(sample(line(100, 100)))
+
+    # convert to pyembroidery format
+    export_pattern = to_pyembroidery(pattern)
+
+    # expect one stitch for each stitch plus one COLOR_BREAK for each block
+    assert len(export_pattern.stitches) == pattern.number_of_stitches + len(pattern.stitch_blocks)
+
+    # check that last stitch is the COLOR_BREAK
+    last_stitch = export_pattern.stitches[-1]
+    assert last_stitch[2] & COMMAND_MASK == COLOR_BREAK
