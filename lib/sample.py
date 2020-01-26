018bc48eb0192a5e392e5b44a23b7776e30eabb8
Tanja Bast <tanja.s.bast@gmail.com>
2020-01-26 12:33:18 +0100

    Add sample generator

--- lib/sample.py
+++ lib/sample.py
@@ -1,3 +1,4 @@
+from functools import partial
 import numpy as np
 
 
@@ -6,3 +7,17 @@ def sample(function, number_of_samples: int, include_endpoint: bool = True):
         number_of_samples += 1
     v = np.linspace(0, 1, num=number_of_samples, endpoint=include_endpoint)
     return np.array([function(t) for t in v])
+
+
+def _generator(function, number_of_samples: int, include_endpoint: bool = True):
+    step = 1 / number_of_samples
+
+    for i in range(number_of_samples):
+        yield function(step * i)
+
+    if include_endpoint:
+        yield function(1)
+
+
+def sample_generator(number_of_samples: int, include_endpoint: bool = True):
+    return partial(_generator, number_of_samples=number_of_samples, include_endpoint=include_endpoint)
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,7 @@
 import numpy as np
 from lib.functions_1d import linear_interpolation, sinus, noise
 from lib.functions_2d import line, circle
-from lib.sample import sample
+from lib.sample import sample, sample_generator
 
 functions = [linear_interpolation(1, 2), sinus(), noise(), circle(), line(10, 20)]
 
@@ -19,3 +19,26 @@ def test_sample():
         assert np.allclose(points_without_endpoint[-1], points_with_endpoint[-2])
 
         assert isinstance(points_with_endpoint, np.ndarray)
+
+
+def test_sample_generator():
+    samples = 10
+    with_endpoint = True
+    gen = sample_generator(samples, with_endpoint)
+
+    # check that sample_generator returns a generator function
+    for f in functions:
+        f_gen = gen(f)
+        v = next(f_gen)
+        assert np.allclose(v, f(0))
+
+    # check that the sample generator returns the same as the sample function
+    for f in functions:
+        # generate a list with the generator
+        f_gen = gen(f)
+        v1 = list(f_gen)
+
+        # retrieve a list from the sample function
+        v2 = sample(f, samples, with_endpoint)
+
+        assert np.allclose(v1, v2)
