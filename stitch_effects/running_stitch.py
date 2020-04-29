7c8d5092497a11744531c014bec856e914e8d978
Tanja Bast <tanja.s.bast@gmail.com>
2020-04-29 17:48:28 +0200

    Implement running stitch by using sample_by_length

--- stitch_effects/running_stitch.py
+++ stitch_effects/running_stitch.py
@@ -1,17 +1,12 @@
-import numpy as np
-
-from lib.functions_1d import linear_interpolation
-from lib.sample import sample
+from lib.functions_2d import line
+from lib.sample import sample_by_length
 
 
-def running_stitch(function, length: float, stitch_length: float, include_last: bool):
+def running_stitch(function, stitch_length: float, include_last: bool):
     assert stitch_length > 0, "Stitch length must be greater than zero"
-    num_stitches = max(int(round(length / stitch_length)), 1)
-    return sample(function, num_stitches, include_last)
+    return sample_by_length(function, stitch_length=stitch_length, include_endpoint=include_last)
 
 
 def running_stitch_line(p1, p2, stitch_length: float, include_last: bool):
-    p1 = np.array(p1)
-    p2 = np.array(p2)
-    distance = np.linalg.norm(p1 - p2)
-    return running_stitch(linear_interpolation(p1, p2), distance, stitch_length, include_last)
+    function = line(p2[0], p2[1], x0=p1[0], y0=p1[1])
+    return running_stitch(function=function, stitch_length=stitch_length, include_last=include_last)
