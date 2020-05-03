897ab70f98d2adfcae731e71bb8ca99d508aeb0e
Tanja Bast <tanja.s.bast@gmail.com>
2020-05-03 18:10:39 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='stitch_generator',
+    version='1.0',
+    description='Stitch generator for embroidery files',
+    author='Tanja Bast',
+    author_email='tanja.s.bast@gmail.com',
+    packages=setuptools.find_packages(),
+    )
