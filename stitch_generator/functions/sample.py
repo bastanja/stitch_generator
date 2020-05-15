8cbf851fbe6bbe6fa22e246ab415ac81f931c544
Tanja Bast <tanja.s.bast@gmail.com>
2020-05-15 18:57:27 +0200

    Add arc_length_mapping function

--- stitch_generator/functions/sample.py
+++ stitch_generator/functions/sample.py
@@ -25,6 +25,19 @@ def length_to_parameter_mapping(function, approximation_samples=1000):
     return length_to_parameter, accumulated[-1]
 
 
+def arc_length_mapping(function, approximation_samples=1000):
+    return arc_length_mapping_with_length(function, approximation_samples)[0]
+
+
+def arc_length_mapping_with_length(function, approximation_samples=1000):
+    parameters = linspace(0, 1, number_of_segments=approximation_samples, include_endpoint=True)
+    accumulated = accumulate_lengths(function(parameters))
+    length = accumulated[-1]
+    accumulated /= length
+    length_to_parameter = interp1d(accumulated, parameters)
+    return length_to_parameter,length
+
+
 def resample(stitches, stitch_length):
     """
     Returns stitches which lie the polyline defined by the parameter stitches. The newly calculated stitches have
