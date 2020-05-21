a4d35c7c6074995871a4574cfbbc01aba53a2b22
Tanja Bast <tanja.s.bast@gmail.com>
2020-05-21 20:18:45 +0200

    Return 1d array from 1d function sequence

--- stitch_generator/functions/function_sequence.py
+++ stitch_generator/functions/function_sequence.py
@@ -6,31 +6,71 @@ from stitch_generator.functions.functions_1d import linear_interpolation
 
 
 def function_sequence(functions, lengths=None):
-    if not lengths:
-        lengths = [estimate_length(f) for f in functions]
+    lengths = _get_lengths(functions, lengths)
+    mapped_functions = _get_mapped_functions(functions, lengths)
+    dimension = _get_dimension(functions[0])
+    if dimension > 1:
+        return _get_sequence(mapped_functions, lengths, dimension)
+    else:
+        return _get_sequence_1d(mapped_functions, lengths)
 
-    lengths = np.cumsum(lengths).astype(float)
-    lengths /= lengths[-1]
 
+def _get_mapped_functions(functions, lengths):
     low = 0
     mapped_functions = []
     for idx, f in enumerate(functions):
         high = lengths[idx]
         mapped_functions.append(combine(linear_interpolation(0, 1, low, high), f))
         low = high
+    return mapped_functions
 
+
+def _get_sequence(mapped_functions, lengths, dimension):
     def sequence(v):
-        v = np.asarray(v, dtype=float).reshape(-1, 1)
-        function_indices = np.argmax(lengths[None, :] >= v, axis=1)
-        dimension = len(mapped_functions[0](0).reshape(-1, 1))
+        function_indices, v = _get_function_indices(v, lengths)
         result = np.zeros((len(v), dimension))
-
-        for j in range(len(mapped_functions)):
-            result[function_indices == j] = mapped_functions[j](v[function_indices == j])
-
+        _evaluate_functions(mapped_functions, function_indices, result, v)
         return result
 
     return sequence
+
+
+def _get_sequence_1d(mapped_functions, lengths):
+    def sequence(v):
+        function_indices, v = _get_function_indices(v, lengths)
+        result = np.zeros_like(v)
+        _evaluate_functions(mapped_functions, function_indices, result, v)
+        return result.T[0]
+
+    return sequence
+
+
+def _evaluate_functions(mapped_functions, function_indices, result, v):
+    for j in range(len(mapped_functions)):
+        result[function_indices == j] = mapped_functions[j](v[function_indices == j])
+
+
+def _get_function_indices(v, lengths):
+    v = np.asarray(v, dtype=float).reshape(-1, 1)
+    function_indices = np.argmax(lengths[None, :] >= v, axis=1)
+    return function_indices, v
+
+
+def _get_dimension(function):
+    dimension = len(function(0).reshape(-1, 1))
+    return dimension
+
+
+def _get_lengths(functions, lengths):
+    if not lengths:
+        try:
+            lengths = [estimate_length(f) for f in functions]
+        except np.AxisError:
+            lengths = [1] * len(functions)
+
+    lengths = np.cumsum(lengths).astype(float)
+    lengths /= lengths[-1]
+    return lengths
 
 
 if __name__ == "__main__":
