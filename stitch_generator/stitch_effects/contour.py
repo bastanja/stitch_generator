bb239eb7b436ec1f157180152b46435c586d5d4c
Tanja Bast <tanja.s.bast@gmail.com>
2020-10-09 20:53:16 +0200

    Avoid duplicate stitches when start and end have no width
    ToDo: make minimal stitch length configurable

--- stitch_generator/stitch_effects/contour.py
+++ stitch_generator/stitch_effects/contour.py
@@ -10,17 +10,27 @@ from stitch_generator.functions.samples import samples_by_length
 
 def contour(path: Path, stitch_length: float):
     left, right = get_boundaries(path)
-    right = inverse(right)
+    return contour_between(left, right, stitch_length=stitch_length, length=estimate_length(path.position))
 
-    length = estimate_length(path.position)
+
+def contour_between(boundary_left, boundary_right, stitch_length: float, length: float):
     t = samples_by_length(total_length=length, segment_length=stitch_length, include_endpoint=False)
-    t_start = samples_by_length(path.width(0).item(), stitch_length, include_endpoint=True)
-    t_end = samples_by_length(path.width(1).item(), stitch_length, include_endpoint=False)
 
-    connect_end = line(origin=left(1)[0], to=right(0)[0])
-    connect_start = line(origin=right(1)[0], to=left(0)[0])
+    start_width = np.linalg.norm(boundary_left(0) - boundary_right(0), axis=1)
+    end_width = np.linalg.norm(boundary_left(1) - boundary_right(1), axis=1)
+    t_start = samples_by_length(start_width.item(), stitch_length, include_endpoint=True)
+    t_end = samples_by_length(end_width.item(), stitch_length, include_endpoint=False)
 
-    stitches = [left(t), connect_end(t_end), right(t), connect_start(t_start)]
+    boundary_right = inverse(boundary_right)
+    connect_end = line(origin=boundary_left(1)[0], to=boundary_right(0)[0])
+    connect_start = line(origin=boundary_right(1)[0], to=boundary_left(0)[0])
+
+    stitches = [boundary_left(t)]
+    if end_width > 0.5:
+        stitches.append(connect_end(t_end))
+    stitches.append(boundary_right(t))
+    if start_width > 0.5:
+        stitches.append(connect_start(t_start))
 
     stitches = np.concatenate(stitches)
 
