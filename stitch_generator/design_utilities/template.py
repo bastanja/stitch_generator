51f4c31485a39f3ed10295dd065865699e9f83dd
Tanja Bast <tanja.s.bast@gmail.com>
2020-10-10 21:46:29 +0200

    Use Path in the template design

--- stitch_generator/design_utilities/template.py
+++ stitch_generator/design_utilities/template.py
@@ -2,8 +2,10 @@ from stitch_generator.design_utilities.embroidery_design import EmbroideryDesign
 from stitch_generator.design_utilities.palette import palette
 from stitch_generator.design_utilities.parameter import FloatParameter
 from stitch_generator.functions.embroidery_pattern import EmbroideryPattern
-from stitch_generator.functions.functions_2d import line
-from stitch_generator.functions.samples import samples_by_length
+from stitch_generator.functions.functions_1d import constant
+from stitch_generator.functions.functions_2d import line, constant_direction
+from stitch_generator.functions.path import Path
+from stitch_generator.stitch_effects.contour import contour
 
 
 class Design(EmbroideryDesign):
@@ -17,9 +19,10 @@ class Design(EmbroideryDesign):
         parameters = self.validate(parameters)
         color = palette()
 
-        f = line((0, 0), (parameters.length, 0))
-        t = samples_by_length(parameters.length, segment_length=parameters.stitch_length, include_endpoint=True)
-        stitches = f(t)
+        path = Path(position=line((0, 0), (parameters.length, 0)), direction=constant_direction(0, -1),
+                    width=constant(1), stroke_alignment=constant(0.5))
+
+        stitches = contour(path=path, stitch_length=parameters.stitch_length)
 
         pattern = EmbroideryPattern()
         pattern.add_stitches(stitches, next(color))
