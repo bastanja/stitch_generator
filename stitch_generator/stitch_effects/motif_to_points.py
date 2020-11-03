cc3d233fbf4076f9b4cee2d325ef1b16d702abf9
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-03 20:09:40 +0100

    Add stitch effect for motif_to_point

--- stitch_generator/stitch_effects/motif_to_points.py
+++ stitch_generator/stitch_effects/motif_to_points.py
@@ -1,25 +1,31 @@
 import itertools
+from functools import partial
 
 import numpy as np
 
 from stitch_generator.functions.place_motif import place_motif_at
 
 
-def motif_to_points(path, motif_position_sampling, line_sampling, motif_generator, length):
-        motif_locations = motif_position_sampling(length)
+def motif_to_points(motif_position_sampling, line_sampling, motif_generator, length):
+    return partial(_motif_to_points, motif_position_sampling=motif_position_sampling, line_sampling=line_sampling,
+                   motif_generator=motif_generator, length=length)
 
-        motifs = [place_motif_at(path.position(t), path.direction(t)[0], path.width(t), next(motif_generator)) for
-                  t in motif_locations]
 
-        fills = []
+def _motif_to_points(path, motif_position_sampling, line_sampling, motif_generator, length):
+    motif_locations = motif_position_sampling(length)
 
-        spaces = zip(motif_locations, motif_locations[1:])
-        for space in spaces:
-            difference = space[1] - space[0]
-            space_length = difference * length
-            samples = line_sampling(space_length) * difference + space[0]
-            fills.append(path.position(samples))
+    motifs = [place_motif_at(path.position(t), path.direction(t)[0], path.width(t), next(motif_generator)) for
+              t in motif_locations]
 
-        combined = [i for i in itertools.chain.from_iterable(itertools.zip_longest(motifs, fills)) if i is not None]
+    fills = []
 
-        return np.concatenate(combined)
+    spaces = zip(motif_locations, motif_locations[1:])
+    for space in spaces:
+        difference = space[1] - space[0]
+        space_length = difference * length
+        samples = line_sampling(space_length) * difference + space[0]
+        fills.append(path.position(samples))
+
+    combined = [i for i in itertools.chain.from_iterable(itertools.zip_longest(motifs, fills)) if i is not None]
+
+    return np.concatenate(combined)
