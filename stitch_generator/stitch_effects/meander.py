ef33afc6ac1f541f9fed95ab17d78f60afa62a00
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-03 21:02:34 +0100

    Use sampling function instead of spacing for satin, double_satin and meander

--- stitch_generator/stitch_effects/meander.py
+++ stitch_generator/stitch_effects/meander.py
@@ -3,32 +3,33 @@ import numpy as np
 from stitch_generator.functions.estimate_length import estimate_length
 from stitch_generator.functions.get_boundaries import get_boundaries
 from stitch_generator.functions.path import Path
-from stitch_generator.functions.samples import linspace
+from stitch_generator.functions.types import ConnectFunction, SamplingFunction
 
 
-def meander(stitch_spacing, connect_function):
-    return lambda path: meander_along(path=path, stitch_spacing=stitch_spacing, connect_function=connect_function,
+def meander(sampling_function: SamplingFunction, connect_function):
+    return lambda path: meander_along(path=path, sampling_function=sampling_function, connect_function=connect_function,
                                       length=estimate_length(path.position))
 
 
-def meander_along(path: Path, stitch_spacing, connect_function, length):
-    return meander_between(*get_boundaries(path), stitch_spacing, connect_function, length)
+def meander_along(path: Path, sampling_function: SamplingFunction, connect_function, length):
+    return meander_between(*get_boundaries(path), sampling_function=sampling_function,
+                           connect_function=connect_function, length=length)
 
 
-def meander_between(boundary_left, boundary_right, stitch_spacing, connect_function, length):
-    points = _meander(boundary_left, boundary_right, stitch_spacing, length)
+def meander_between(boundary_left, boundary_right, sampling_function: SamplingFunction,
+                    connect_function: ConnectFunction, length):
+    points = _meander(boundary_left, boundary_right, sampling_function=sampling_function, length=length)
     connection = [connect_function(points[i - 1], points[i]) for i in range(1, len(points), 2)]
     return np.concatenate(connection)
 
 
-def _meander(boundary_left, boundary_right, stitch_spacing, length):
-    num_parameters = int(round(length / stitch_spacing))
-    parameters = linspace(0, 1, num_parameters, include_endpoint=True)
-    stitches = np.zeros((len(parameters) * 2, 2))
+def _meander(boundary_left, boundary_right, sampling_function: SamplingFunction, length):
+    t = sampling_function(length)
+    stitches = np.zeros((len(t) * 2, 2))
 
-    stitches[0::4] = boundary_left(parameters[0::2])
-    stitches[1::4] = boundary_right(parameters[0::2])
-    stitches[2::4] = boundary_right(parameters[1::2])
-    stitches[3::4] = boundary_left(parameters[1::2])
+    stitches[0::4] = boundary_left(t[0::2])
+    stitches[1::4] = boundary_right(t[0::2])
+    stitches[2::4] = boundary_right(t[1::2])
+    stitches[3::4] = boundary_left(t[1::2])
 
     return stitches
--- stitch_generator/stitch_effects/satin.py
+++ stitch_generator/stitch_effects/satin.py
@@ -3,26 +3,22 @@ import numpy as np
 from stitch_generator.functions.estimate_length import estimate_length
 from stitch_generator.functions.get_boundaries import get_boundaries
 from stitch_generator.functions.path import Path
-from stitch_generator.functions.sampling import fixed_sampling_with_offset
-from stitch_generator.functions.types import ConnectFunction, Function2D
+from stitch_generator.functions.types import ConnectFunction, Function2D, SamplingFunction
 from stitch_generator.stitch_effects.zigzag import zigzag
 
 
-def satin(stitch_spacing, connect_function: ConnectFunction):
-    return lambda path: satin_along(path=path, stitch_spacing=stitch_spacing, connect_function=connect_function,
+def satin(sampling_function: SamplingFunction, connect_function: ConnectFunction):
+    return lambda path: satin_along(path=path, sampling_function=sampling_function, connect_function=connect_function,
                                     length=estimate_length(path.position))
 
 
-def satin_along(path: Path, stitch_spacing: float, connect_function: ConnectFunction, length: float):
-    return satin_between(*get_boundaries(path), stitch_spacing=stitch_spacing, connect_function=connect_function,
+def satin_along(path: Path, sampling_function: SamplingFunction, connect_function: ConnectFunction, length: float):
+    return satin_between(*get_boundaries(path), sampling_function=sampling_function, connect_function=connect_function,
                          length=length)
 
 
-def satin_between(boundary_left: Function2D, boundary_right: Function2D, stitch_spacing: float,
+def satin_between(boundary_left: Function2D, boundary_right: Function2D, sampling_function: SamplingFunction,
                   connect_function: ConnectFunction, length: float):
-    sampling_function = fixed_sampling_with_offset(stitch_length=stitch_spacing, alignment=0, offset=0,
-                                                   include_endpoint=True)
-
     points = zigzag(boundary_left, boundary_right, sampling_function, length)
     connection = [connect_function(points[i - 1], points[i]) for i in range(1, len(points))]
     return np.concatenate(connection)
