effd3bf9e6f79b5fd5dbe662435659e00908e03b
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-03 20:35:07 +0100

    Add type hints to sampling function

--- stitch_generator/stitch_effects/repeat_along.py
+++ stitch_generator/stitch_effects/repeat_along.py
@@ -4,14 +4,15 @@ from stitch_generator.functions.function_1d_stairs import stairs
 from stitch_generator.functions.function_modifiers import repeat, mix
 from stitch_generator.functions.get_boundaries import get_boundaries
 from stitch_generator.functions.path import Path
+from stitch_generator.functions.types import SamplingFunction
 
 
-def repeat_along(path: Path, repetitions: int, sampling_function, length: float, step_ratio: float):
+def repeat_along(path: Path, repetitions: int, sampling_function: SamplingFunction, length: float, step_ratio: float):
     return repeat_between(*get_boundaries(path), repetitions=repetitions, sampling_function=sampling_function,
                           length=length, step_ratio=step_ratio)
 
 
-def repeat_between(boundary_left, boundary_right, repetitions: int, sampling_function, length: float,
+def repeat_between(boundary_left, boundary_right, repetitions: int, sampling_function: SamplingFunction, length: float,
                    step_ratio: float):
     boundary_left = repeat(r=repetitions, function=boundary_left, mode='reflect')
     boundary_right = repeat(r=repetitions, function=boundary_right, mode='reflect')
@@ -21,7 +22,7 @@ def repeat_between(boundary_left, boundary_right, repetitions: int, sampling_fun
 
     sampling_length = 1 / repetitions
 
-    t = [sampling_function(total_length=length) * sampling_length + i * sampling_length for i in range(repetitions)]
+    t = [sampling_function(length) * sampling_length + i * sampling_length for i in range(repetitions)]
     t.append([1])
     t = np.concatenate(t)
 
