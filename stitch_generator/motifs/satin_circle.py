a0fbe4a5832d02ea9d795673d88739f39c80617a
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-03 22:06:04 +0100

    Add satin circle motif

---
+++ stitch_generator/motifs/satin_circle.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from stitch_generator.functions.connect_functions import running_stitch_line
+from stitch_generator.functions.estimate_length import estimate_length
+from stitch_generator.functions.function_modifiers import repeat, scale
+from stitch_generator.functions.functions_1d import circular_arc, constant
+from stitch_generator.functions.functions_2d import line, constant_direction
+from stitch_generator.functions.path import Path
+from stitch_generator.functions.samples import samples_by_length
+from stitch_generator.functions.sampling import fixed_sampling_with_offset
+from stitch_generator.stitch_effects.satin import satin
+from stitch_generator.stitch_effects.underlay import contour_zigzag_underlay
+
+
+def satin_circle(diameter: float, stitch_length: float, pull_compensation: float = 0, underlay_inset: float = 0.5,
+                 underlay_spacing: float = 1.5, satin_spacing: float = 0.2, return_to_start: bool = True):
+    underlay_effect = contour_zigzag_underlay(inset=underlay_inset, stitch_length=stitch_length,
+                                              spacing=underlay_spacing)
+    satin_effect = satin(
+        sampling_function=fixed_sampling_with_offset(stitch_length=satin_spacing, include_endpoint=True,
+                                                     offset=0.5, alignment=0, minimal_segment_size=0.5),
+        connect_function=running_stitch_line(diameter * 2, include_endpoint=False))
+
+    path = Path(
+        position=line((0, 0), (diameter, 0)),
+        direction=constant_direction(0, -1),
+        width=scale(diameter + pull_compensation, repeat(2, circular_arc(), mode='reflect')),
+        stroke_alignment=constant(0.5)
+    )
+
+    stitches = []
+    if return_to_start:
+        stitches.append(
+            path.position(samples_by_length(estimate_length(path.position), stitch_length, include_endpoint=False)))
+        path = path.inverse()
+    else:
+        stitches.append(path.position(0))
+
+    stitches += [underlay_effect(path), satin_effect(path)]
+    return np.concatenate(stitches)
