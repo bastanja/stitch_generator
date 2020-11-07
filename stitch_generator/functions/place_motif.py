297f3f2ab8507b4bfbb6874c64e6301f03443531
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-07 21:21:28 +0100

    Add option to skip endpoint of motif

--- stitch_generator/functions/place_motif.py
+++ stitch_generator/functions/place_motif.py
@@ -1,11 +1,12 @@
 from stitch_generator.stitch_effects.rotate import rotate
 
 
-def place_motif_at(position, direction, scale, stitch_pattern):
+def place_motif_at(position, direction, scale, stitch_pattern, include_endpoint=False):
     if len(stitch_pattern) == 0:
         return position
     stitch_pattern = stitch_pattern.copy()
     stitch_pattern *= scale
     stitch_pattern = rotate(stitches=stitch_pattern, sin=direction[1], cos=direction[0])
     stitch_pattern += position
-    return stitch_pattern
+    last = None if include_endpoint else -1
+    return stitch_pattern[0:last]
