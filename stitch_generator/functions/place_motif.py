0e1600ce5c93bf4ac6ec3abebc70a765cf2fda7a
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-15 15:05:31 +0100

    Add function to place a motif between two points

--- stitch_generator/functions/place_motif.py
+++ stitch_generator/functions/place_motif.py
@@ -1,12 +1,47 @@
+import numpy as np
+
 from stitch_generator.stitch_effects.rotate import rotate
 
 
-def place_motif_at(position, direction, scale, stitch_pattern, include_endpoint=False):
-    if len(stitch_pattern) == 0:
+def place_motif_at(position, direction, scale, motif, include_endpoint=False):
+    if len(motif) == 0:
         return position
-    stitch_pattern = stitch_pattern.copy()
-    stitch_pattern *= scale
-    stitch_pattern = rotate(stitches=stitch_pattern, sin=direction[1], cos=direction[0])
-    stitch_pattern += position
+    motif = motif.copy()
+    motif *= scale
+    motif = rotate(stitches=motif, sin=direction[1], cos=direction[0])
+    motif += position
     last = None if include_endpoint else -1
-    return stitch_pattern[0:last]
+    return motif[0:last]
+
+
+def place_motif_between(p1, p2, motif, include_endpoint=False, width_scale=None):
+    if len(motif) == 0:
+        return p1
+
+    motif = _normalize_motif(motif)
+    delta = p2 - p1
+    scale_x = np.linalg.norm(delta)
+
+    scale_y = scale_x
+    if width_scale:
+        scale_y *= width_scale
+
+    delta /= scale_x
+
+    motif *= (scale_x, scale_y)
+    motif = rotate(motif, delta[1], delta[0])
+    motif += p1
+
+    last = None if include_endpoint else -1
+    return motif[0:last]
+
+
+def _normalize_motif(motif):
+    motif = motif.copy()
+    p1, p2 = motif[0], motif[-1]
+    d = p2 - p1
+    size = np.linalg.norm(d)
+    motif -= p1
+    motif /= size
+    d /= size
+    return rotate(motif, -d[1], d[0])
