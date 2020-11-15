1f705f77d571ead327f5bb657483ae1b8f7579dc
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-15 15:07:30 +0100

    Add stitch effect motif_to_segments.py

---
+++ stitch_generator/stitch_effects/motif_to_segment.py
@@ -0,0 +1,23 @@
+from functools import partial
+
+import numpy as np
+
+from stitch_generator.functions.path import Path
+from stitch_generator.functions.place_motif import place_motif_between
+from stitch_generator.functions.types import SamplingFunction
+
+
+def motif_to_segments(motif_position_sampling: SamplingFunction, motif_generator, length: float):
+    return partial(_motif_to_segments, motif_position_sampling=motif_position_sampling, motif_generator=motif_generator,
+                   length=length)
+
+
+def _motif_to_segments(path: Path, motif_position_sampling: SamplingFunction, motif_generator, length):
+    motif_boundaries = motif_position_sampling(length)
+    motif_points = path.position(motif_boundaries)
+
+    motifs = [place_motif_between(p1, p2, next(motif_generator)) for p1, p2 in zip(motif_points, motif_points[1:])]
+
+    motifs.append([motif_points[-1]])
+
+    return np.concatenate(motifs)
