2d60f07a44b3d0d5ae543cb522a675aed4f542f1
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-21 15:23:21 +0100

    Extract sample_by_length to separate file

---
+++ stitch_generator/sampling/sample_by_length.py
@@ -0,0 +1,15 @@
+from stitch_generator.functions.types import Array1D, SamplingFunction
+from stitch_generator.sampling.sample import sample
+
+
+def sample_by_length(total_length: float, segment_length: float, include_endpoint: bool) -> Array1D:
+    return sample(total_length=total_length, segment_length=segment_length, alignment=0,
+                  include_endpoint=include_endpoint, offset=0, minimal_segment_size=0)
+
+
+def sampling_by_length(segment_length: float, include_endpoint: bool) -> SamplingFunction:
+    def f(total_length: float):
+        return sample_by_length(total_length=total_length, segment_length=segment_length,
+                                include_endpoint=include_endpoint)
+
+    return f
