60153233e1876878f690265ed16df7addec64361
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-22 15:38:35 +0100

    Update docstring

--- stitch_generator/functions/types.py
+++ stitch_generator/functions/types.py
@@ -4,6 +4,7 @@ import numpy as np
 
 Array1D = np.ndarray
 Array2D = np.ndarray
+
 Point = Union[Tuple[float], List[float], np.ndarray]
 
 Function1D = Callable[[Union[float, Array1D]], Array1D]
@@ -12,6 +13,6 @@ Function2D = Callable[[Union[float, Array1D]], Array2D]
 # A function that is called with a length value and returns an array of floats between 0 and 1
 SamplingFunction = Callable[[float], Array1D]
 
-# A Function that is called with a start and end point and returns an Array2D of points starting at the start point and
+# A function that is called with a start and end point and returns an Array2D of points starting at the start point and
 # ending at the end point
 ConnectFunction = Callable[[Point, Point], Array2D]
