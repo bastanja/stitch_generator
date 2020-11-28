abb7e54a60e0f9803584cd49a9111448f18791d9
Tanja Bast <tanja.s.bast@gmail.com>
2020-11-28 18:29:02 +0100

    Add factor to change width of stitch effect

--- stitch_generator/designs/stitch_effects.py
+++ stitch_generator/designs/stitch_effects.py
@@ -4,7 +4,8 @@ from stitch_generator.design_utilities.embroidery_design import EmbroideryDesign
 from stitch_generator.design_utilities.palette import palette
 from stitch_generator.design_utilities.parameter import FloatParameter
 from stitch_generator.functions.embroidery_pattern import EmbroideryPattern
-from stitch_generator.functions.functions_1d import constant
+from stitch_generator.functions.function_modifiers import mix, scale
+from stitch_generator.functions.functions_1d import constant, arc
 from stitch_generator.functions.functions_2d import line, constant_direction
 from stitch_generator.path.path import Path
 from stitch_generator.stitch_effects.collection import stitch_effects
@@ -14,16 +15,19 @@ class Design(EmbroideryDesign):
     def __init__(self):
         EmbroideryDesign.__init__(self, name="stitch_effects", parameters={
             'stitch_length': FloatParameter("Stitch length", 1, 3, 6),
-            'length': FloatParameter("Length", 10, 80, 200)
+            'length': FloatParameter("Length", 10, 80, 200),
+            'width_factor': FloatParameter("Width Factor", 0, 0, 1)
         })
 
     def get_pattern(self, parameters):
         parameters = self.validate(parameters)
         color = palette()
 
+        width_f = mix(constant(10), scale(10, arc()), constant(parameters.width_factor))
+
         path = Path(position=line((0, 0), (parameters.length, 0)),
                     direction=constant_direction(0, -1),
-                    width=constant(10),
+                    width=width_f,
                     stroke_alignment=constant(0.5))
 
         pattern = EmbroideryPattern()
