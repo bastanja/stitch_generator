d215fc95843fb7e264517463c0b920272885f478
Tanja Bast <tanja.s.bast@gmail.com>
2020-12-05 13:34:10 +0100

    Export svg without scaling

--- stitch_generator/file_io/embroidery_export.py
+++ stitch_generator/file_io/embroidery_export.py
@@ -4,8 +4,7 @@ from pyembroidery import EmbPattern, write_vp3, write_svg, \
 from stitch_generator.functions.embroidery_pattern import EmbroideryPattern
 
 
-def to_pyembroidery(embroidery_pattern: EmbroideryPattern):
-    scale_factor = 10  # convert from millimeters to embroidery file scale 1/10 mm
+def to_pyembroidery(embroidery_pattern: EmbroideryPattern, scale_factor: float = 10):
     result = EmbPattern()
 
     for i in range(len(embroidery_pattern.stitch_blocks)):
@@ -19,8 +18,9 @@ export_parameters = {"tie_on": CONTINGENCY_TIE_ON_THREE_SMALL, "tie_off": CONTIN
 
 
 def export_vp3(pattern: EmbroideryPattern, file_path: str):
-    write_vp3(to_pyembroidery(pattern), file_path, export_parameters)
+    scale_factor = 10  # convert from millimeters to embroidery file scale 1/10 mm
+    write_vp3(to_pyembroidery(pattern, scale_factor=scale_factor), file_path, export_parameters)
 
 
 def export_svg(pattern: EmbroideryPattern, file_path: str):
-    write_svg(to_pyembroidery(pattern), file_path)
+    write_svg(to_pyembroidery(pattern, scale_factor=1), file_path)
