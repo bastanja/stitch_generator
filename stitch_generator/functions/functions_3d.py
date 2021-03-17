c729ad3d89eed347f3c9b01d6921f3c00377eeb7
Tanja Bast <tanja.s.bast@gmail.com>
2021-03-17 22:00:10 +0100

    Support 3D Functions

---
+++ stitch_generator/functions/functions_3d.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from stitch_generator.functions.ensure_shape import ensure_1d_shape
+from stitch_generator.utilities.types import Function3D, Function1D
+
+
+def function_3d(fx: Function1D, fy: Function1D, fz: Function1D) -> Function3D:
+    def f(t):
+        t = ensure_1d_shape(t)
+        return np.array([fx(t), fy(t), fz(t)]).T
+
+    return f
--- stitch_generator/utilities/types.py
+++ stitch_generator/utilities/types.py
@@ -4,11 +4,13 @@ import numpy as np
 
 Array1D = np.ndarray
 Array2D = np.ndarray
+Array3D = np.ndarray
 
 Point = Union[Tuple[float], List[float], np.ndarray]
 
 Function1D = Callable[[Union[float, Array1D]], Array1D]
 Function2D = Callable[[Union[float, Array1D]], Array2D]
+Function3D = Callable[[Union[float, Array1D]], Array3D]
 
 # A function that is called with a length value and returns an array of floats between 0 and 1
 SamplingFunction = Callable[[float], Array1D]
