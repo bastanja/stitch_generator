0a649c655e66e50638a23f7a2a45c98a918fec74
Tanja Bast <tanja.s.bast@gmail.com>
2021-03-18 21:17:28 +0100

    Add function to ensure two dimensional shape of array

--- stitch_generator/functions/ensure_shape.py
+++ stitch_generator/functions/ensure_shape.py
@@ -6,3 +6,10 @@ def ensure_1d_shape(parameters):
     parameters = np.squeeze(parameters)
     parameters = np.atleast_1d(parameters)
     return parameters
+
+
+def ensure_2d_shape(parameters):
+    parameters = np.atleast_2d(parameters)
+    parameters = np.squeeze(parameters)
+    parameters = np.atleast_2d(parameters)
+    return parameters
