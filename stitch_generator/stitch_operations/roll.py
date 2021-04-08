b147170d7c440088679f7a636e1bcc9d087fd6f0
Tanja Bast <tanja.s.bast@gmail.com>
2021-04-08 20:38:31 +0200

    Add stitch operation roll to move start of a closed ring of stitches

---
+++ stitch_generator/stitch_operations/roll.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def roll(stitches, amount):
+    modified = stitches
+    modified[1::] = np.roll(stitches[1::], amount, axis=0)
+    modified[0] = modified[-1]
+    return modified
