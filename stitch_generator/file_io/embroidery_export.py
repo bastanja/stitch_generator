6beda26bed35b985827545e999db976a279c9e6c
Tanja Bast <tanja.s.bast@gmail.com>
2021-05-02 21:01:31 +0200

    Allow export of any supported file format, list writable formats

--- stitch_generator/file_io/embroidery_export.py
+++ stitch_generator/file_io/embroidery_export.py
@@ -1,5 +1,5 @@
-from pyembroidery import EmbPattern, write_vp3, write_svg, \
-    CONTINGENCY_TIE_ON_THREE_SMALL, CONTINGENCY_TIE_OFF_THREE_SMALL
+from pyembroidery import EmbPattern, write_vp3, write_svg, CONTINGENCY_TIE_ON_THREE_SMALL, \
+    CONTINGENCY_TIE_OFF_THREE_SMALL
 
 from stitch_generator.framework.embroidery_pattern import EmbroideryPattern
 
@@ -20,6 +20,23 @@ export_parameters = {"tie_on": CONTINGENCY_TIE_ON_THREE_SMALL, "tie_off": CONTIN
 def export_vp3(pattern: EmbroideryPattern, file_path: str):
     scale_factor = 10  # convert from millimeters to embroidery file scale 1/10 mm
     write_vp3(to_pyembroidery(pattern, scale_factor=scale_factor), file_path, export_parameters)
+
+
+def export_pattern(pattern: EmbroideryPattern, file_path: str):
+    """
+    Exports an EmbroideryPattern to a file
+    Args:
+        pattern: The EmbroideryPattern to write
+        file_path: The file path including the file extension. The extension defines the written file format.
+    """
+    scale_factor = 10  # convert from millimeters to embroidery file scale 1/10 mm
+    pyembroidery_pattern = to_pyembroidery(pattern, scale_factor=10)
+    EmbPattern.static_write(pyembroidery_pattern, file_path, export_parameters)
+
+
+def writable_formats():
+    formats = [f for f in EmbPattern.supported_formats() if "writer" in f]
+    return [{f["extension"]: f["description"]} for f in formats]
 
 
 def export_svg(pattern: EmbroideryPattern, file_path: str):
