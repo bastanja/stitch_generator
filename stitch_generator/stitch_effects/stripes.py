2f64385851a7064297447813f728286dea8eab21
Tanja Bast <tanja.s.bast@gmail.com>
2021-07-04 11:45:52 +0200

    Add default value for step ratio

--- stitch_generator/stitch_effects/stripes.py
+++ stitch_generator/stitch_effects/stripes.py
@@ -3,6 +3,6 @@ from stitch_generator.stitch_effects.utilities.stripes import stripes_along
 from stitch_generator.utilities.types import SamplingFunction
 
 
-def stripes(repetitions: int, sampling_function: SamplingFunction, step_ratio: float) -> StitchEffect:
+def stripes(repetitions: int, sampling_function: SamplingFunction, step_ratio: float = 0.1) -> StitchEffect:
     return lambda path: stripes_along(path, repetitions=repetitions, sampling_function=sampling_function,
                                       step_ratio=step_ratio)
