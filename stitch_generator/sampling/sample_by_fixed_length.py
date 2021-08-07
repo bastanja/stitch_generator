68604dac2bca416c01ea929b046041505c78a75e
Tanja Bast <tanja.s.bast@gmail.com>
2021-08-07 12:25:06 +0200

    Add function for sampling without start and endpoint

--- stitch_generator/sampling/sample_by_fixed_length.py
+++ stitch_generator/sampling/sample_by_fixed_length.py
@@ -1,5 +1,5 @@
+from stitch_generator.sampling.sample import sample, sample_inner
 from stitch_generator.utilities.types import Array1D, SamplingFunction
-from stitch_generator.sampling.sample import sample
 
 
 def sample_by_fixed_length(total_length: float, segment_length: float, include_endpoint: bool, alignment: float = 0,
@@ -14,5 +14,14 @@ def sampling_by_fixed_length(segment_length: float, include_endpoint: bool, alig
         return sample_by_fixed_length(total_length=total_length, segment_length=segment_length,
                                       include_endpoint=include_endpoint, alignment=alignment,
                                       offset=offset, minimal_segment_size=minimal_segment_size)
+
+    return f
+
+
+def inner_sampling_by_fixed_length(segment_length: float, alignment: float = 0,
+                                   offset: float = 0, minimal_segment_size: float = 0) -> SamplingFunction:
+    def f(total_length: float):
+        return sample_inner(total_length=total_length, segment_length=segment_length, alignment=alignment,
+                            offset=offset, minimal_segment_size=minimal_segment_size)
 
     return f
