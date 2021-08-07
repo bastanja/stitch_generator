0ad7e905a5679ac9b99d0fe814418e6a71033a8e
Tanja Bast <tanja.s.bast@gmail.com>
2021-08-07 12:23:44 +0200

    Allow different corner radii for rounded rectangle

--- stitch_generator/shapes/rounded_rect.py
+++ stitch_generator/shapes/rounded_rect.py
@@ -1,19 +1,29 @@
-from stitch_generator.functions.function_modifiers import repeat, shift, add
+import itertools
+
+from stitch_generator.functions.function_modifiers import repeat, multiply, inverse
 from stitch_generator.functions.function_sequence import function_sequence
-from stitch_generator.functions.functions_2d import line, ellipse, constant_direction
+from stitch_generator.functions.functions_2d import constant_direction
+from stitch_generator.shapes.ellipse import ellipse
+from stitch_generator.shapes.line import line
 
 
-def rounded_rect(width: float, height: float, corner_radius_x: float, corner_radius_y: float):
-    corner = repeat(0.25, ellipse(corner_radius_x, corner_radius_y))
+def corner(x, y, w, h):
+    result = repeat(0.25, ellipse(w, h, center=(x - w, y - h)))
+    return result
 
-    down = line((width, corner_radius_y), (width, height - corner_radius_y))
-    left = line((width - corner_radius_x, height), (corner_radius_x, height))
-    up = line((0, height - corner_radius_y), (0, corner_radius_y))
-    right = line((corner_radius_x, 0), (width - corner_radius_x, 0))
 
-    corner1 = add(corner, constant_direction(width - corner_radius_x, height - corner_radius_y))
-    corner2 = shift(1, add(corner, constant_direction(corner_radius_x, height - corner_radius_y)))
-    corner3 = shift(2, add(corner, constant_direction(corner_radius_x, corner_radius_y)))
-    corner4 = shift(3, add(corner, constant_direction(width - corner_radius_x, corner_radius_y)))
+def rounded_rect_with_radius(width: float, height: float, corner_radius_x: float, corner_radius_y: float):
+    return rounded_rect(width, height, [(corner_radius_x, corner_radius_y)] * 4)
 
-    return function_sequence((down, corner1, left, corner2, up, corner3, right, corner4))
+
+def rounded_rect(width: float, height: float, corner_radii):
+    corners = [corner(width / 2, height / 2, *radius) for radius in corner_radii]
+
+    direction = itertools.cycle((lambda x: x, inverse))
+    scale_x = (1, -1, -1, 1)
+    scale_y = (1, 1, -1, -1)
+    corners = [d(multiply(c, constant_direction(x, y))) for d, c, x, y in zip(direction, corners, scale_x, scale_y)]
+    connections = [line(a(1), b(0)) for a, b in zip([corners[-1]] + corners[:-1], corners)]
+    functions = list(itertools.chain.from_iterable(zip(connections, corners)))
+
+    return function_sequence(functions)
