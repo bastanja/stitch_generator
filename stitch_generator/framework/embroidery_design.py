2fe308e6faf30ecf98ad26715d749224a17d1de0
Tanja Bast <tanja.s.bast@gmail.com>
2021-10-23 21:00:59 +0200

    Document EmbroideryDesign

--- stitch_generator/framework/embroidery_design.py
+++ stitch_generator/framework/embroidery_design.py
@@ -4,7 +4,21 @@ from stitch_generator.framework.cli import get_parser, write_pattern_to_file
 
 
 class EmbroideryDesign:
+    """
+    An EmbroideryDesign creates EmbroideryPatterns based on input parameters.
+
+    While an EmbroideryPattern is static and contains only stitch positions, an EmbroideryDesign can dynamically change
+    based on parameters. For example a design may have parameters for size, stitch length, stitch density etc. Based on
+    the values of these parameters, different EmbroideryPatterns can be created from the same EmbroideryDesign.
+    """
+
     def __init__(self, name: str, parameters: dict):
+        """
+        Args:
+            name: The name of the design. Used as filename when exporting the design as embroidery file.
+            parameters: A dictionary that maps parameter names to parameters like FloatParameter, IntParameter etc.
+                These parameters can be used in the get_pattern function of the EmbroideryDesign
+        """
         self.parameters = parameters
         self.name = name
 
