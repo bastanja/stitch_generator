6de2d1f8443d713b96d07ae19c827a5db4796367
Tanja Bast <tanja.s.bast@gmail.com>
2021-10-25 21:30:38 +0200

    Copy stitches in roll function

--- stitch_generator/stitch_operations/roll.py
+++ stitch_generator/stitch_operations/roll.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def roll(stitches, amount):
-    modified = stitches
+    modified = stitches.copy()
     modified[1::] = np.roll(stitches[1::], amount, axis=0)
     modified[0] = modified[-1]
     return modified
