a8c5abee3a5289fb51dd3ec103d044804dd811cd
Tanja Bast <tanja.s.bast@gmail.com>
2021-10-25 21:07:28 +0200

    Add satin ellipse

--- stitch_generator/motifs/satin_circle.py
+++ stitch_generator/motifs/satin_circle.py
@@ -11,27 +11,28 @@ from stitch_generator.stitch_effects.satin import satin
 from stitch_generator.stitch_effects.underlay_contour_zigzag import underlay_contour_zigzag
 
 
-def satin_circle(diameter: float, stitch_length: float, pull_compensation: float = 0, underlay_inset: float = 0.5,
-                 underlay_spacing: float = 1.5, satin_spacing: float = 0.2, return_to_start: bool = True):
+def satin_ellipse(width: float, height: float, stitch_length: float, pull_compensation: float = 0,
+                  underlay_inset: float = 0.5, underlay_spacing: float = 1.5, satin_spacing: float = 0.2,
+                  return_to_start: bool = True):
     underlay_effect = underlay_contour_zigzag(inset=underlay_inset, stitch_length=stitch_length,
                                               spacing=underlay_spacing)
 
     satin_effect = satin(
         sampling_function=sampling_by_length_with_offset(segment_length=satin_spacing, offset=0.5),
-        connect_function=running_stitch_line(diameter * 2, include_endpoint=False))
+        connect_function=running_stitch_line(width * 2, include_endpoint=False))
 
-    radius = diameter / 2
+    radius = height / 2
 
     path = Path(
         shape=line((-radius, 0), (radius, 0)),
         direction=constant_direction(0, -1),
-        width=scale(diameter + pull_compensation, repeat(2, circular_arc, mode='reflect')),
+        width=scale(width + pull_compensation, repeat(2, circular_arc, mode='reflect')),
         stroke_alignment=constant(0.5)
     )
 
     stitches = []
     if return_to_start:
-        stitches.append(path.shape(sample_by_length(diameter, stitch_length)))
+        stitches.append(path.shape(sample_by_length(height, stitch_length)))
         path = path.inverse()
     else:
         stitches.append(path.shape(0))
@@ -39,3 +40,11 @@ def satin_circle(diameter: float, stitch_length: float, pull_compensation: float
     stitches += [underlay_effect(path), satin_effect(path)]
     stitches.append(path.shape(1))
     return np.concatenate(stitches)
+
+
+def satin_circle(diameter: float, stitch_length: float, pull_compensation: float = 0, underlay_inset: float = 0.5,
+                 underlay_spacing: float = 1.5, satin_spacing: float = 0.2, return_to_start: bool = True):
+    return satin_ellipse(width=diameter, height=diameter, stitch_length=stitch_length,
+                         pull_compensation=pull_compensation, underlay_inset=underlay_inset,
+                         underlay_spacing=underlay_spacing, satin_spacing=satin_spacing,
+                         return_to_start=return_to_start)
