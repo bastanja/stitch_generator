762b7d79e18290220e7619afa444c47d8ab70a6a
Tanja Bast <tanja.s.bast@gmail.com>
2021-11-01 17:58:31 +0100

    Remove satin design

--- stitch_generator/designs/design_collection.py
+++ stitch_generator/designs/design_collection.py
@@ -1,15 +1,13 @@
-from stitch_generator.designs import satin_samples
 from stitch_generator.designs import motif_stitches
 from stitch_generator.designs import noise_spiral
-from stitch_generator.designs import satin
+from stitch_generator.designs import satin_samples
 from stitch_generator.designs import stitch_effects
 from stitch_generator.designs import waves
 
 designs = [
-    satin_samples.Design,
     motif_stitches.Design,
+    noise_spiral.Design,
+    satin_samples.Design,
     stitch_effects.Design,
-    satin.Design,
-    waves.Design,
-    noise_spiral.Design
+    waves.Design
 ]
