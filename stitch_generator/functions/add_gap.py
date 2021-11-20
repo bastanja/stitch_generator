fddd92dfde8e24160c4f68453dc2014637c97df7
Tanja Bast <tanja.s.bast@gmail.com>
2021-11-20 22:30:25 +0100

    Document function

--- stitch_generator/functions/add_gap.py
+++ stitch_generator/functions/add_gap.py
@@ -1,9 +1,21 @@
 from stitch_generator.functions.estimate_length import estimate_length
 from stitch_generator.functions.function_modifiers import repeat, shift
 from stitch_generator.functions.functions_1d import linear_interpolation
+from stitch_generator.utilities.types import Function2D
 
 
-def add_gap(function, gap_offset_mm, gap_length_mm):
+def add_gap(function: Function2D, gap_offset_mm, gap_length_mm) -> Function2D:
+    """
+    Takes a closed shape function and adds a gap to it. This can be used e.g. for turning openings
+
+    Args:
+        function: A 2DFunction describing a closed shape, e.g. a circle
+        gap_offset_mm: The distance from the shape start to the start of the gap in millimeters
+        gap_length_mm: The length of the gap in millimeters
+
+    Returns:
+        A 2D function that starts after the end of the gap, goes around the shape and ends at the start of the gap
+    """
     mm_to_offset = linear_interpolation(0, 1, source_low=0, source_high=estimate_length(function))
 
     # make it circular
