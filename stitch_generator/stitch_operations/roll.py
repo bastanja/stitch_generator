1e26c3b273d2735f26ee928c02fbc0a28c9f15c1
Tanja Bast <tanja.s.bast@gmail.com>
2022-01-01 11:59:19 +0100

    Adapt roll funtion for non-closed shapes

--- stitch_generator/stitch_operations/roll.py
+++ stitch_generator/stitch_operations/roll.py
@@ -1,8 +1,18 @@
 import numpy as np
 
 
-def roll(stitches, amount):
-    modified = stitches.copy()
-    modified[1::] = np.roll(stitches[1::], amount, axis=0)
-    modified[0] = modified[-1]
+def roll(points, amount):
+    # exclude the first point from the roll operation if it is the same as the last point
+    start_index = 1 if np.allclose(points[0], points[-1]) else 0
+
+    # copy is necessary to avoid changing the original points
+    modified = points.copy()
+
+    # roll by amount
+    modified[start_index::] = np.roll(points[start_index::], amount, axis=0)
+
+    # if the first and last point were equal, make them equal again
+    if start_index > 0:
+        modified[0] = modified[-1]
+
     return modified
