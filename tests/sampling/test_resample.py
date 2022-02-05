93eb03a4781c1aba5bdf16f21fdf8410fc52cfae
Tanja Bast <tanja.s.bast@gmail.com>
2022-02-05 21:14:15 +0100

    Remove sampling presets

--- tests/sampling/test_resample.py
+++ tests/sampling/test_resample.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 from stitch_generator.sampling.resample import resample, resample_with_sampling_function
-from stitch_generator.sampling.sample_by_number import sample_by_number
-from stitch_generator.sampling.sampling_presets import sampling_presets_stateless
+from stitch_generator.sampling.sample_by_fixed_length import sampling_by_fixed_length
+from stitch_generator.sampling.sample_by_length import sampling_by_length
+from stitch_generator.sampling.sample_by_number import sample_by_number, sampling_by_number
 from stitch_generator.shapes.line import line
 
 
@@ -19,13 +20,19 @@ def test_resample():
     assert np.allclose(resampled, f(sample_by_number(50)))
 
 
+def sampling_functions():
+    yield sampling_by_length(segment_length=3)
+    yield sampling_by_fixed_length(segment_length=2, alignment=0, offset=0)
+    yield sampling_by_number(number_of_segments=3)
+
+
 def test_resample_with_sampling_function():
     total_length = 10
 
     f = line((0, 0), (total_length, 0))
     stitches = f(sample_by_number(100))
 
-    for sampling_function in iter(sampling_presets_stateless(alignment=0)):
+    for sampling_function in iter(sampling_functions()):
         direct_samples = f(sampling_function(total_length))
         resampled = resample_with_sampling_function(stitches, sampling_function)
         assert np.allclose(direct_samples, resampled)
