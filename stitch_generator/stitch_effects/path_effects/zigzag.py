d2cd1dd3dfb9126ad53ebe226443c6c810472490
Tanja Bast <tanja.s.bast@gmail.com>
2022-05-01 18:52:50 +0200

    Fix double_zigzag, add itype hints

--- stitch_generator/stitch_effects/path_effects/zigzag.py
+++ stitch_generator/stitch_effects/path_effects/zigzag.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 from stitch_generator.framework.path import Path, get_boundaries
+from stitch_generator.framework.stitch_effect import StitchEffect
 from stitch_generator.framework.types import Function2D, SamplingFunction, Array2D
 from stitch_generator.sampling.sample_by_length import regular
 
 
-def zigzag(spacing_function: SamplingFunction):
+def zigzag(spacing_function: SamplingFunction) -> StitchEffect:
     return lambda path: zigzag_along(path=path, spacing_function=spacing_function)
 
 
@@ -27,12 +28,12 @@ def zigzag_between(boundary_left: Function2D, boundary_right: Function2D, spacin
     return stitches
 
 
-def double_zigzag(spacing_function: SamplingFunction):
-    return lambda path: zigzag_along(path=path, spacing_function=spacing_function)
+def double_zigzag(spacing_function: SamplingFunction) -> StitchEffect:
+    return lambda path: double_zigzag_along(path=path, spacing_function=spacing_function)
 
 
 def double_zigzag_along(path: Path, spacing_function: SamplingFunction) -> Array2D:
-    return zigzag_between(*get_boundaries(path), spacing_function=spacing_function, length=path.length)
+    return double_zigzag_between(*get_boundaries(path), spacing_function=spacing_function, length=path.length)
 
 
 def double_zigzag_between(boundary_left: Function2D, boundary_right: Function2D, spacing_function: SamplingFunction,
