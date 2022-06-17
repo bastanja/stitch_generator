4b8adfbbacbb3df473ff9931f79fa01cae3509a8
Tanja Bast <tanja.s.bast@gmail.com>
2022-06-17 16:29:14 +0200

    Avoid duplicate points in tile_motif

--- stitch_generator/stitch_effects/path_effects/tile_motif.py
+++ stitch_generator/stitch_effects/path_effects/tile_motif.py
@@ -2,6 +2,7 @@ from stitch_generator.framework.path import Path
 from stitch_generator.framework.stitch_effect import StitchEffect
 from stitch_generator.framework.types import Array2D
 from stitch_generator.stitch_effects.utilities.motif_to_path import motif_to_path
+from stitch_generator.stitch_operations.remove_duplicates import remove_duplicates
 from stitch_generator.stitch_operations.tile import tile_x
 
 
@@ -13,4 +14,6 @@ def tile_motif_along(path: Path, motif: Array2D, motif_length: float):
     repetitions = int(round(path.length / motif_length))
     motif_tiled = tile_x(motif=motif, spacing=1, repetitions=repetitions)
     motif_tiled[:, 0] /= repetitions
-    return motif_to_path(motif_tiled, path)
+    stitches = motif_to_path(motif_tiled, path)
+    stitches = remove_duplicates(stitches)
+    return stitches
