ff65058f5022bfa0d31d7e0f425435f76b6d2930
Tanja Bast <tanja.s.bast@gmail.com>
2022-07-10 15:10:20 +0200

    Add half_smoothstep

--- stitch_generator/collection/functions/functions_1d.py
+++ stitch_generator/collection/functions/functions_1d.py
@@ -1,7 +1,7 @@
 from stitch_generator.functions.function_1d_stairs import stairs
 from stitch_generator.functions.function_modifiers import subtract, repeat, chain
 from stitch_generator.functions.functions_1d import constant, linear_interpolation, arc, circular_arc, sinus, \
-    cubic_interpolation
+    cubic_interpolation, smoothstep
 from stitch_generator.sampling.sample_by_number import sample_by_number
 
 linear_0_1 = linear_interpolation(0, 1)
@@ -13,6 +13,8 @@ half_positive_sine = repeat(0.5, positive_sine)
 half_positive_cosine = repeat(0.5, positive_cosine)
 
 half_peak = subtract(constant(1.0), repeat(0.5, arc, mode='reflect'))
+half_smoothstep = chain(repeat(0.5, smoothstep), linear_interpolation(0, 1, source_low=0, source_high=0.5))
+
 half_circle = repeat(2, circular_arc, mode='reflect')
 
 cubic_arc = cubic_interpolation(((0, 0), (0.5, 1), (1, 0)))
