a278529ef1df39bd280d7dabbb77ed4c4bcef098
Tanja Bast <tanja.s.bast@gmail.com>
2022-08-09 20:44:52 +0200

    Simplify motif_to_points, avoid duplicate points

--- stitch_generator/stitch_effects/shape_effects/motif_to_points.py
+++ stitch_generator/stitch_effects/shape_effects/motif_to_points.py
@@ -5,6 +5,8 @@ import numpy as np
 from stitch_generator.framework.stitch_effect import StitchEffect
 from stitch_generator.framework.types import SamplingFunction, Array2D, Function2D
 from stitch_generator.functions.estimate_length import estimate_length
+from stitch_generator.sampling.samples_between import samples_between
+from stitch_generator.sampling.sampling_modifiers import remove_start, remove_end
 from stitch_generator.stitch_effects.utilities.place_motif import place_motif_at
 
 
@@ -20,41 +22,32 @@ def motif_to_points_on_shape(shape: Function2D, direction: Function2D, motif_pos
     total_length = estimate_length(shape)
     motif_locations = motif_position_sampling(total_length)
 
-    if motif_locations.size > 0:
-        motifs = [place_motif_at(shape(t), direction(t)[0], 1, next(motif_generator)) for t in motif_locations]
-        starts_with_motif = np.isclose(motif_locations[0], 0)
+    # If there are no motifs, sample the whole shape with the sampling function
+    if motif_locations.size == 0:
+        return shape(line_sampling(total_length))
 
-    else:
-        motifs = []
-        starts_with_motif = False
+    # place a motif at each motif location along the shape
+    motifs = [place_motif_at(shape(t), direction(t)[0], 1, next(motif_generator), include_endpoint=True) for t in
+              motif_locations]
 
-    motif_locations = add_first_and_last(motif_locations)
+    # fill all spaces between the motifs
+    inner_sampling = remove_start(remove_end(line_sampling))
+    fills = [shape(samples_between(total_length, start, end, inner_sampling)) for start, end in
+             zip(motif_locations, motif_locations[1:])]
 
-    fills = []
-    spaces = zip(motif_locations, motif_locations[1:])
-    for space in spaces:
-        difference = space[1] - space[0]
-        space_length = difference * total_length
-        samples = line_sampling(space_length) * difference + space[0]
-        fills.append(shape(samples))
-    fills.append(shape(1))
+    # fill the space before the first motif, if there is any
+    first_fill = None
+    if motif_locations[0] > 0:
+        first_fill = shape(samples_between(total_length, 0, motif_locations[0], remove_end(line_sampling)))
 
-    if starts_with_motif:
-        parts = itertools.zip_longest(motifs, fills)
-    else:
-        parts = itertools.zip_longest(fills, motifs)
+    # fill the space after the last motif, if there is any
+    last_fill = None
+    if motif_locations[-1] < 1:
+        last_fill = shape(samples_between(total_length, motif_locations[-1], 1, remove_start(line_sampling)))
 
-    combined = [i for i in itertools.chain.from_iterable(parts) if i is not None]
+    # combine all fills to one list
+    fills = [first_fill] + fills + [last_fill]
 
+    # interleave fills and motifs
+    combined = [i for i in itertools.chain.from_iterable(itertools.zip_longest(fills, motifs)) if i is not None]
     return np.concatenate(combined)
-
-
-def add_first_and_last(samples):
-    if samples.size == 0:
-        return np.array((0.0, 1))
-    if not np.isclose(samples[0], 0):
-        samples = np.concatenate(([0], samples))
-    if not np.isclose(samples[-1], 1):
-        samples = np.concatenate((samples, [1]))
-
-    return samples
