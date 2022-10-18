b65222ab9e8cfaee8fa64d67968f418d144b3124
Tanja Bast <tanja.s.bast@gmail.com>
2022-10-18 16:48:35 +0200

    Fix typing import

--- stitch_generator/shapes/spiral.py
+++ stitch_generator/shapes/spiral.py
@@ -1,4 +1,4 @@
-from collections import Sequence
+from typing import Sequence
 
 from stitch_generator.functions.function_modifiers import repeat, mix
 from stitch_generator.functions.functions_1d import linear_interpolation
