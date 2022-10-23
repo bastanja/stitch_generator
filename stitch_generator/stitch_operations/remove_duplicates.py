7bcc1f89595e9d05fd2f14eb077f428c42166d72
Tanja Bast <tanja.s.bast@gmail.com>
2022-10-23 17:34:48 +0200

    Avoid removal of the first stitch

--- stitch_generator/stitch_operations/remove_duplicates.py
+++ stitch_generator/stitch_operations/remove_duplicates.py
@@ -15,8 +15,11 @@ def remove_duplicates(points: Array2D, threshold: float = 0.01) -> Array2D:
     Returns:
         The points without duplicates
     """
+    if len(points) < 1:
+        return points
 
     to_previous = points - np.roll(points, 1, 0)
     distance_to_previous = np.linalg.norm(to_previous, axis=1)
     long_enough = distance_to_previous > threshold
+    long_enough[0] = True  # never skip the first point
     return points[long_enough]
